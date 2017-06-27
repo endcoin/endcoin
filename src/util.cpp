75e06ec81e0436a756c05fd6ff31159402048359
endcoin <dejanmaas@gmail.com>
2017-06-27 21:14:12 +0200

    12

--- src/util.cpp
+++ src/util.cpp
@@ -970,29 +970,63 @@ void PrintExceptionContinue(std::exception* pex, const char* pszThread)
 }
 
 boost::filesystem::path GetDefaultDataDir()
-{
-    namespace fs = boost::filesystem;
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\EndCoin    // Windows >= Vista: C:\Users\Username\AppData\Roaming\EndCoin    // Mac: ~/Library/Application Support/EndCoin    // Unix: ~/.EndCoin#ifdef WIN32
-    // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "EndCoin";
-#else if
-    fs::path pathRet;
-    char* pszHome = getenv("HOME");
-    if (pszHome == NULL || strlen(pszHome) == 0)
-        pathRet = fs::path("/");
-    else
-        pathRet = fs::path(pszHome);
-#ifdef MAC_OSX
-    // Mac
-    pathRet /= "Library/Application Support";
-    fs::create_directory(pathRet);
-    return pathRet / "EndCoin";
-#else if
-    // Unix
-    return pathRet / ".EndCoin";
-#endif
-#endif
-}
+{ 
+    namespace fs = boost::filesystem; 
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\endcoin 
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\endcoin 
+    // Mac: ~/Library/Application Support/endcoin 
+    // Unix: ~/.endcoin 
+ #ifdef WIN32 
+     // Windows 
+     return GetSpecialFolderPath(CSIDL_APPDATA) / "EndCoin"; 
+ #else 
+         //2-2-2017 
+       fs::path pathRet; 
+        char* pszHome = getenv("HOME"); 
+
+ 
+        if (mapArgs.count("-datadir")) 
+        { 
+            fs::path path2015 = fs::system_complete(mapArgs["-datadir"]); 
+            if (fs::is_directory(path2015)) 
+            { 
+                pathRet = path2015; 
+             } 
+         } 
+        else 
+         { 
+             if (pszHome == NULL || strlen(pszHome) == 0) 
+                pathRet = fs::path("/"); 
+            else 
+                 pathRet = fs::path(pszHome); 
+         } 
+ #ifdef MAC_OSX 
+    // Mac 
+     pathRet /= "Library/Application Support"; 
+     fs::create_directory(pathRet); 
+ 
+ 
+     if (mapArgs.count("-datadir")) 
+     { 
+         return pathRet; 
+     } 
+     else 
+     { 
+         return pathRet / "EndCoin"; 
+     } 
+ #else 
+     // Unix 
+     if (mapArgs.count("-datadir")) 
+     { 
+         return pathRet; 
+     } 
+     else 
+     { 
+         return pathRet / ".EndCoin"; 
+     } 
+#endif 
+#endif 
+} 
 
 const boost::filesystem::path &GetDataDir(bool fNetSpecific)
 {
